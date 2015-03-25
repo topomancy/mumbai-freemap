2d9421a0462f4441a3e9bb81a473f412adf296c0
Sanjay B <b@pad.ma>
2015-03-26 01:06:58 +0530

    add some search fields to property

--- cts/admin.py
+++ cts/admin.py
@@ -7,7 +7,7 @@ from leaflet.admin import LeafletGeoAdmin
 class PropertyAdmin(LeafletGeoAdmin):
     list_display = ('__unicode__', 'street_locality', 'tenure', 'ground_rent_due', 'laughton_no', 'area_sqm',)
     list_filter = ('stable',)
-    search_fields = ('street_locality',)
+    search_fields = ('street_locality', 'division', 'cs_sheet_no',)
     default_lon = 72.855211097628413
     default_lat = 19.415775291486027
     default_zoom = 4
