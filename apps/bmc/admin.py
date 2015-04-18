69c429b212d24e1f155a44c0685fa5f4f3cb21b6
Sanjay B <b@pad.ma>
2015-04-18 17:14:01 +0530

    admin for bmc data

--- apps/bmc/admin.py
+++ apps/bmc/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from models import *
+from leaflet.admin import LeafletGeoAdmin
 
-# Register your models here.
+
+class HolderHistoryInline(admin.StackedInline):
+    model = HolderHistory
+
+
+class LeaseholdPlotAdmin(LeafletGeoAdmin):
+    search_fields = ('lessee', 'cs_no', 'estate_scheme__name', 'asset_no',)
+    list_display = ('lessee', 'cs_no', 'estate_scheme', 'asset_no',)
+    list_filter = ('estate_scheme',)
+    inlines = [HolderHistoryInline]
+
+
+class EstateSchemeAdmin(LeafletGeoAdmin):
+    search_fields = ('name',)
+
+
+admin.site.register(LeaseholdPlot, LeaseholdPlotAdmin)
+admin.site.register(EstateScheme, EstateSchemeAdmin)
